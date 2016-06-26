b8cb1d72f3b77841964db117fd104aabe70c71e3
admin <fyrsenko@gmail.com>
2016-06-26 17:51:45 +0300

    Done

--- src/ua/artcode/week1/contactlist/TestContactList.java
+++ src/ua/artcode/week1/contactlist/TestContactList.java
@@ -17,40 +17,7 @@ public class TestContactList {
         //contactList.addContact(contact3);
 
 
-        String json = "{" +
-                "  \"list\": [" +
-                "    {" +
-                "      \"name\": \"Ivan\"," +
-                "      \"phone\": \"0934443223\"," +
-                "      \"age\": \"20\"," +
-                "      \"address\": {" +
-                "        \"city\": \"Kiyv\"," +
-                "        \"street\": \"Lepsa\"," +
-                "        \"number\": \"20\"" +
-                "      }" +
-                "    }," +
-                "    {" +
-                "      \"name\": \"Vlad\"," +
-                "      \"phone\": \"0956720990\"," +
-                "      \"age\": \"22\"," +
-                "      \"address\": {" +
-                "        \"city\": \"Odesa\"," +
-                "        \"street\": \"Naberezhna\"," +
-                "        \"number\": \"33\"" +
-                "      }" +
-                "    }," +
-                "    {" +
-                "      \"name\": \"Alex\"," +
-                "      \"phone\": \"0504332209\"," +
-                "      \"age\": \"18\"," +
-                "      \"address\": {" +
-                "        \"city\": \"Kiyv\"," +
-                "        \"street\": \"Lesi\"," +
-                "        \"number\": \"2\"" +
-                "      }" +
-                "    }" +
-                "  ]" +
-                "}";
+        String json = "{\"list\":[{\"name\":\"Ivan\",\"phone\":\"0934443223\",\"age\":\"20\",\"address\":{\"city\":\"Kiyv\",\"street\":\"Lepsa\",\"number\":\"20\"}},{\"name\":\"Vlad\",\"phone\":\"0956720990\",\"age\":\"22\",\"address\":{\"city\":\"Odesa\",\"street\":\"Naberezhna\",\"number\":\"33\"}},{\"name\":\"Alex\",\"phone\":\"0504332209\",\"age\":\"18\",\"address\":{\"city\":\"Kiyv\",\"street\":\"Lesi\",\"number\":\"2\"}}]}";
 
         //String [] temp = json.split("\\[");
         //temp = temp[1].split("\\{");
