090066c9ff1fba256323f40f5167b6d97a5433c2
admin <fyrsenko@gmail.com>
2016-08-09 21:40:07 +0300

    week6 working with MyHashtable

--- src/ua/artcode/week6/MyHashtable.java
+++ src/ua/artcode/week6/MyHashtable.java
@@ -12,6 +12,7 @@ public class MyHashtable<K, V> implements Map<K, V> {
     public static final int DEFAULT_TABLE_SIZE = 32;
 
     private Node[] table;
+
     private int size;
 
     public MyHashtable() {
@@ -42,37 +43,91 @@ public class MyHashtable<K, V> implements Map<K, V> {
 
     @Override
     public V get(Object key) {
-        return null;
+
+        int hash = key.hashCode();
+
+        Node node = table[hash % table.length];
+
+        if (node == null) return null;
+
+        V value = tryGet(node, (K) key);
+
+        return value;
+    }
+
+    private V tryGet(Node node, K key) {
+
+        if (node.key.equals(key)) {
+            return (V) node.value;
+        }
+
+        if (node.next == null) {
+            return null;
+        }
+
+        return tryGet(node.next, key);
+
     }
 
     @Override
     public V put(K key, V value) {
+
         int hash = key.hashCode();
+
         if (table[hash % table.length] == null) {
             table[hash % table.length] = new Node(key, value, null);
             size++;
             return value;
         }
 
-        boolean operationSuccessfully = tryPut(table[hash % table.length], value);
+        boolean operationSuccessfully = tryPut(table[hash % table.length], key, value);
 
-        return operationSuccessfully? value : null;
+        return operationSuccessfully ? value : null;
     }
 
-    private boolean tryPut(Node node, V value) {
+    private boolean tryPut(Node node, K key, V value) {
 
-        if(node.value.equals(value)) return false;
-
-        if(node.next==null) {
-            node.next = node;
+        if (node.key.equals(key)) {
+            node.value = value;
             return true;
         }
 
-        return tryPut(node.next, value);
+        if (node.next == null) {
+            node.next = new Node(key, value, null);
+            size++;
+            return true;
+        }
+
+        return tryPut(node.next, key, value);
     }
 
     @Override
     public V remove(Object key) {
+
+        V value;
+        int hash = key.hashCode();
+        Node node = table[hash%table.length];
+
+        if(node==null) return null;
+
+        if(node.key.equals(key)) {
+            value = (V) node.value;
+            table[hash%table.length] = node.next;
+            size--;
+            return value;
+        }
+
+        value = tryRemoveFromNextNode(node.next, (K) key);
+
+
+        return null;
+    }
+
+    private V tryRemoveFromNextNode (Node node, K key) {
+        if(node==null) return null;
+        if(key.equals(node.key)) {
+
+        }
         return null;
     }
 
@@ -102,7 +157,7 @@ public class MyHashtable<K, V> implements Map<K, V> {
         return null;
     }
 
-    private class Node<K,V> {
+    private class Node<K, V> {
         K key;
         V value;
         Node next;
--- src/ua/artcode/week6/test/TestMyHashMap.java
+++ src/ua/artcode/week6/test/TestMyHashMap.java
@@ -11,16 +11,22 @@ public class TestMyHashMap {
 
     public static void main(String[] args) {
         Map<String, Integer> map = new MyHashtable<>();
-        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+
+
 
         map.put("1",1);
-        map.put("2",2);
-        map.put("3",3);
-        map.put("4",4);
-        map.put("2",3);
-        map.put("2",6);
 
-        System.out.println(map.size());
+        map.put("2",2);
+        map.put("2",3);
+        map.put("3",32);
+
+        int testValue = map.get("3");
+        System.out.println(testValue);
+
+
+
+
 
     }
 }
